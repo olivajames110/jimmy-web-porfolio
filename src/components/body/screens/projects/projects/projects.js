93fe1bf02a889b2099b5ee9eae364242dff025d1
olivajames110 <olivajames110@gmail.com>
2019-07-11 15:33:45 -0400

    Added device containers on projects page

--- src/components/body/screens/projects/projects/projects.js
+++ src/components/body/screens/projects/projects/projects.js
@@ -1,6 +1,15 @@
-import React, {Component, Fragment} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCloudSunRain, faScroll, faPencilAlt, faChartBar, faMoneyBill} from '@fortawesome/free-solid-svg-icons';
+import React, { Component, Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCloudSunRain,
+	faScroll,
+	faPencilAlt,
+	faChartBar,
+	faMoneyBill,
+	faMobileAlt,
+	faTabletAlt,
+	faDesktop
+} from '@fortawesome/free-solid-svg-icons';
 import Form from '../form/form';
 import PriceCalc from '../priceCalc/priceCalc';
 import Home from '../../home/home';
@@ -10,94 +19,143 @@ class Projects extends Component {
 	state = {
 		currentProject: '',
 		currentProjectName: 'Choose a project above!',
-		viewType: ''
+		deviceType: 'desktop-container'
 	};
 
 	handleFormChange = () => {
 		this.setState({
-			currentProject: <Form/>,
+			currentProject: <Form />,
 			currentProjectName: 'Form'
 		});
 	};
 
 	handleWeatherChange = () => {
 		this.setState({
-			currentProject: <Home/>,
+			currentProject: <Home />,
 			currentProjectName: 'Drawing Board'
 		});
 	};
 
 	handlePriceCalcChange = () => {
 		this.setState({
-			currentProject: <PriceCalc/>,
+			currentProject: <PriceCalc />,
 			currentProjectName: 'Price Calculator'
 		});
 	};
 
 	handleChange() {
-		return <div className="project-screen-cont fade-in">{this.state.currentProject}</div>
+		return <div className="project-screen-cont fade-in">{this.state.currentProject}</div>;
 	}
 
-	handleCheckActive(p) {
-		return (this.state.currentProjectName == p
-			? 'project-is-active'
-			: '')
+	handleCheckActive(area, state) {
+		if (area == 'project') {
+			return this.state.currentProjectName == state ? 'project-is-active' : '';
+		} else if (area == 'device') return this.state.deviceType == state ? 'navigation-option--active' : '';
 	}
+
+	handleDeviceChange = (e) => {
+		console.log(e.currentTarget.getAttribute('value'));
+		let device = e.currentTarget.getAttribute('value');
+		console.log(device);
+		this.setState({ deviceType: device });
+		document.getElementById('device-project-container').className = device;
+	};
 
 	render() {
 		return (
 			<Fragment>
-				<div
-					style={{
-					height: this.state.currentProject == ''
-						? '100%'
-						: '70%'
-				}}
-					className="interior-body">
+				<div className="interior-body">
 					<div id="projects" className="screen-section-container">
 						<h1 className="screen-section-container-title">Projects</h1>
 						<p className="screen-section-container-desc">
-							Here are a few examples of some small projects that I have been working on for
-							fun. Please don't judge, none of them are fully complete, this is more of a
-							playground where I can work and experiment with them. Enjoy!
+							Here are a few examples of some small projects that I have been working on for fun. Please
+							don't judge, none of them are fully complete, this is more of a playground where I can work
+							and experiment with them. Enjoy!
 						</p>
 						<div className="project-btn-container">
 							<div onClick={this.handleWeatherChange} className="project-btn">
 								<div className="project-btn-icon">
-									<FontAwesomeIcon icon={faPencilAlt}/>
+									<FontAwesomeIcon icon={faPencilAlt} />
 								</div>
 								<div className="project-btn-text">Drawing Board</div>
 							</div>
 							<div
 								state={this.state.currentProject}
 								onClick={this.handleFormChange}
-								className={`project-btn ${this.handleCheckActive('Form')}`}>
-								<div className="project-btn-icon"/>
-								<FontAwesomeIcon icon={faScroll}/>
+								className={`project-btn ${this.handleCheckActive('project', 'Form')}`}
+							>
+								<div className="project-btn-icon" />
+								<FontAwesomeIcon icon={faScroll} />
 								<div className="project-btn-text">Form</div>
 							</div>
 							<div className="project-btn">
-								<div className="project-btn-icon"/>
-								<FontAwesomeIcon icon={faChartBar}/>
+								<div className="project-btn-icon" />
+								<FontAwesomeIcon icon={faChartBar} />
 								<div className="project-btn-text">Dashboard</div>
 							</div>
 							<div className="project-btn">
-								<div className="project-btn-icon"/>
-								<FontAwesomeIcon icon={faCloudSunRain}/>
+								<div className="project-btn-icon" />
+								<FontAwesomeIcon icon={faCloudSunRain} />
 								<div className="project-btn-text">Weather App</div>
 							</div>
 							<div
 								onClick={this.handlePriceCalcChange}
-								className={`project-btn ${this.handleCheckActive('Price Calculator')}`}>
-								<div className="project-btn-icon"/>
-								<FontAwesomeIcon icon={faMoneyBill}/>
+								className={`project-btn ${this.handleCheckActive('project', 'Price Calculator')}`}
+							>
+								<div className="project-btn-icon" />
+								<FontAwesomeIcon icon={faMoneyBill} />
 								<div className="project-btn-text">Price Calc</div>
 							</div>
 						</div>
 					</div>
-					<div className="current-project">{this.state.currentProjectName}</div>
 				</div>
-				{this.handleChange()}
+				<div className="wrapper">
+					<div className="navigation">
+						<div className="device-orientation-container">
+							<div className="navigation-title"> {this.state.currentProjectName}</div>
+							<div className="device-orientation-btns-container">
+								<div
+									onClick={(e) => this.handleDeviceChange(e)}
+									id="iphone-device-button"
+									value="iphone-container"
+									className={`navigation-option  ${this.handleCheckActive(
+										'device',
+										'iphone-container'
+									)}`}
+								>
+									<FontAwesomeIcon icon={faMobileAlt} />
+								</div>
+								<div
+									onClick={(e) => this.handleDeviceChange(e)}
+									id="ipad-device-button"
+									value="ipad-container"
+									className={`navigation-option  ${this.handleCheckActive(
+										'device',
+										'ipad-container'
+									)}`}
+								>
+									<FontAwesomeIcon icon={faTabletAlt} />
+								</div>
+								<div
+									onClick={(e) => this.handleDeviceChange(e)}
+									id="desktop-device-button"
+									value="desktop-container"
+									className={`navigation-option  ${this.handleCheckActive(
+										'device',
+										'desktop-container'
+									)}`}
+								>
+									<FontAwesomeIcon icon={faDesktop} />
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="project-holder">
+						<div id="device-project-container" className="desktop-container">
+							{this.handleChange()}
+						</div>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
