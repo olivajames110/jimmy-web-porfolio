c667e8d67b5c7e80583a2f8e181e525094739b5a
olivajames110 <olivajames110@gmail.com>
2019-07-31 14:04:36 -0400

    Fixed mobile

--- src/components/screens/projectsScreen/projects/project/project.js
+++ src/components/screens/projectsScreen/projects/project/project.js
@@ -2,27 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './project.css';
 
-const Project = (props) => {
+const Project = props => {
 	return (
-		<span onClick={props.handlePriceCalcChange} className={`project-btn `}>
+		<span
+			onClick={props.handlePriceCalcChange}
+			className={`project-btn ${props.handleCheckActive('project', props.projectName)}`}
+		>
 			<div className="project-btn-icon">
 				<FontAwesomeIcon icon={props.icon} />
 			</div>
-			<div className={`project-btn-text ${props.handleCheckActive('project', props.projectName)}`}>
-				{props.projectName}
-			</div>
+			<div className="project-btn-text">{props.projectName}</div>
 		</span>
 	);
 };
-
-// const Project = (props) => {
-// 	return (
-// 		<div className="project-btn">
-// 			<div className="project-btn-icon" />
-// 			<FontAwesomeIcon icon={this.props.icon} />
-// 			<div className="project-btn-text">{this.props.buttonText}</div>
-// 		</div>
-// 	);
-// };
 
 export default Project;
