554e10ca49dd5f253b3681be5a8af8c16b7f80ec
olivajames110 <olivajames110@gmail.com>
2019-12-11 22:53:59 -0500

    Grillmarx V1 Finished

--- src/assets/map/map.js
+++ src/assets/map/map.js
@@ -1,10 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import './map.css';
 const apiKey = 'AIzaSyB-NdQtol6W5tmk6AzHsKKTgRThZd9njbk';
 
 const Map = props => {
-	// const map = new window.google.maps.Map();
+	const mapRef = useRef();
 
-	return <div className={`map ${props.className} style=${props.style}`}>Map</div>;
+	useEffect(
+		() => {
+			const map = new window.google.maps.Map(mapRef.current, {
+				center: props.c3,
+				zoom: props.zoom
+			});
+
+			new window.google.maps.Marker({ position: props.center, map: map });
+			new window.google.maps.Marker({ position: props.c2, map: map });
+			new window.google.maps.Marker({ position: props.c3, map: map });
+		},
+		[props.c2, props.c3, props.center, props.zoom]
+	);
+
+	return (
+		<div id="map-container">
+			<div ref={mapRef} className={`map ${props.className} style=${props.style}`}>
+				Map
+			</div>
+			<div id="map-container__locations">
+				<div className="location">
+					<h2 className="location__title">Olney Location</h2>
+					<p>18149 Town Center Drive</p>
+					<p>Olney, MD 20832</p>
+					<p>Tel: 301-570-1111</p>
+				</div>
+				<div className="location">
+					<h2 className="location__title">Olney Location</h2>
+					<p>18149 Town Center Drive</p>
+					<p>Olney, MD 20832</p>
+					<p>Tel: 301-570-1111</p>
+				</div>
+				<div className="location">
+					<h2 className="location__title">Olney Location</h2>
+					<p>18149 Town Center Drive</p>
+					<p>Olney, MD 20832</p>
+					<p>Tel: 301-570-1111</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Map;
